82cec5389feca83e7de15bb05eadec67c4abd341
macostacos <ivanov.maxim0@yandex.ru>
2025-04-03 13:19:11 +0300

    fix: path for cors fixed

--- src/main/java/com/example/gateway/config/CorsGlobalConfiguration.java
+++ src/main/java/com/example/gateway/config/CorsGlobalConfiguration.java
@@ -15,7 +15,7 @@ public class CorsGlobalConfiguration {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
 
