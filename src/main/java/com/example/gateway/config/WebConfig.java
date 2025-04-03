91f616c599204b0e896c7a9a37c4e3554704c2a8
macostacos <ivanov.maxim0@yandex.ru>
2025-04-03 12:50:42 +0300

    feat: WebConfig created

---
+++ src/main/java/com/example/gateway/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.gateway.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
